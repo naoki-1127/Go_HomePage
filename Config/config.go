bd78faf6a9bf8390bd28ac87ede2da3a52c3c888
naoki-1127 <73074569+naoki-1127@users.noreply.github.com>
2022-01-02 16:59:45 +0900

    get log

---
+++ Config/config.go
@@ -0,0 +1,25 @@
+package Config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
+type ConfigList struct {
+	LogFile string
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Printf("Failed to read file: %v", err)
+		os.Exit(1)
+	}
+	Config = ConfigList{
+		LogFile: cfg.Section("LogSettings").Key("log_file").String(),
+	}
+}
--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	Config "Go_app/Config"
+	Utils "Go_app/Utils"
 	"log"
 	"net/http"
 
@@ -8,12 +10,14 @@ import (
 )
 
 func main() {
+	Utils.LoggingSetting(Config.Config.LogFile)
+	log.Println("Sample")
 	serve()
 }
 
 func serve() {
+	log.Println("サーバーを起動しました")
 	router := gin.Default()
-
 	router.Static("/Views", "./Views")
 
 	router.StaticFS("/goapp/login", http.Dir("./views/static"))
