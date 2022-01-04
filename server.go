4704bf51af25ae1a90e76b3b1b99ecc9340d631a
naoki-1127 <73074569+naoki-1127@users.noreply.github.com>
2022-01-05 02:13:28 +0900

    add front

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	Config "Go_app/Config"
+	Controllers "Go_app/Controllers"
 	Utils "Go_app/Utils"
 	"log"
 	"net/http"
@@ -18,9 +19,20 @@ func main() {
 func serve() {
 	log.Println("サーバーを起動しました")
 	router := gin.Default()
+
+	/* 	router.LoadHTMLGlob("Views/static/*.html")
+
+	router.GET("/goapp", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	}) */
 	router.Static("/Views", "./Views")
 
-	router.StaticFS("/goapp/login", http.Dir("./views/static"))
+	router.StaticFS("/goapp", http.Dir("./views/static"))
+
+	v1 := router.Group("/api/v1")
+	{
+		v1.POST("/register", Controllers.CreateUserAction)
+	}
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal("Server Run Failed.: ", err)
